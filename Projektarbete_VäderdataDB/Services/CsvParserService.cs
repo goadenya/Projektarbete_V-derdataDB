565119e75d4f1a10940c1bfb563655803f5a8120
dderiba <55302543+dderiba@users.noreply.github.com>
2021-01-29 18:39:56 +0100

    Fixed

--- Projektarbete_VäderdataDB/Services/CsvParserService.cs
+++ Projektarbete_VäderdataDB/Services/CsvParserService.cs
@@ -11,26 +11,30 @@ namespace Projektarbete_VäderdataDB.Services
 {
     class CsvParserService
     {
-        public void SeedData(EFContext Context)
-        {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            const string resourceName = @"E:\OneDrive - IT-Högskolan Sverige AB\source\repos\Projektarbete_VäderdataDB\Projektarbete_VäderdataDB\Models\Temperature.cs";
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
-                {
-                    CsvReader csvReader = new CsvReader((IParser)reader);
-                    //csvReader.Configuration.WillThrowOnMissingField = false;
-                    var records = csvReader.GetRecords<Temperature>().ToArray();
+        /// <summary>
+        /// Legacy Code - Alternativt sätt att läsa in CSV filer
+        /// </summary>
+        /// <param name="Context"></param>
+        //public void SeedData(EFContext Context) 
+        //{
+        //    Assembly assembly = Assembly.GetExecutingAssembly();
+        //    const string resourceName = @"E:\OneDrive - IT-Högskolan Sverige AB\source\repos\Projektarbete_VäderdataDB\Projektarbete_VäderdataDB\Models\Temperature.cs";
+        //    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+        //    {
+        //        using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+        //        {
+        //            CsvReader csvReader = new CsvReader((IParser)reader);
+        //            //csvReader.Configuration.WillThrowOnMissingField = false;
+        //            var records = csvReader.GetRecords<Temperature>().ToArray();
 
-                    foreach (Temperature record in records)
-                    {
-                        //record.Datum = 
-                        Context.Temperatures.Add(record);
-                    }
-                }
-            }
-            Context.SaveChanges();
-        }
+        //            foreach (Temperature record in records)
+        //            {
+        //                //record.Datum = 
+        //                Context.Temperatures.Add(record);
+        //            }
+        //        }
+        //    }
+        //    Context.SaveChanges();
+        //}
     }
 }
